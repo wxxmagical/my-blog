f1b7aeb4766ffe8c62670a3fd7ae69c144b72637
wxxmagical <wxxmagical@gmail.com>
2024-03-11 22:25:20 +0800

    引入pxtorem插件，解决插件配置加载报错的问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions :{
+          plugins: [
+            require('postcss-pxtorem')({
+                rootValue: 16,
+                unitPrecision: 5,
+                propList: ['*'],
+                selectorBlackList: [],
+                replace: true,
+                mediaQuery: false,
+                minPixelValue: 0,
+                exclude: /node_modules/i
+            }),
+          ]
+        }
+
+      }
+    }
+  },
 })
