ac8985f75bb8c3e900f38654a5a4704da0488843
preAdmin <preadmin@163.com>
2024-08-29 18:15:05 +0800

    原型链和原型对象和闭包和防抖

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,26 +1,16 @@
-
-// export function debounce (callback){
-//     let t
-//     console.log(t,"ttt")
-//     if (t !== null) {
-//         clearTimeout(t);
-//       }
-//       t = setTimeout(() => {
-//         callback
-//       }, 500);
-// },
-
-export function debounce(func, delay) {
-    let timeout;
-    console.log(func)
-    return function(...args) {
-        debugger
-        console.log(11)
-        const context = this;
-        if (timeout) clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            console.log(args,'sssss')
-            func.apply(context, args);
-      }, delay);
-    };
-  }
+export function debounce(func, wait) {
+  console.log("timeOut")  //只打印一次，只有在debounce定义的时候打印一次。下面同理
+  let timeout;  //只执行一次
+  return function(...args) {
+    //防抖函数会返回到debounce调用处，所以，这里的args是默认穿的resize默认参数
+      const context = this;//指向window
+      console.log("timeOut1")
+      if (timeout) {
+          clearTimeout(timeout);
+      }
+      timeout = setTimeout(function() {
+        // console.log(this,'this11')
+          func.apply(this,args);  //这里如果不修改func函数的this,那么func函数的this为undefined
+      }, wait);
+  };
+}
--- src/views/goStudy/components/demo.vue
+++ src/views/goStudy/components/demo.vue
@@ -9,18 +9,35 @@
       <div>2</div>
       <div>3</div>
     </div>
+    <div class="sendMessage" @click="sendMessage">点击发送消息</div>
+    <div>{{ getMessage }}</div>
   </div>
 </template>
 
 <script>
+Function.prototype.say = function () {
+  console.log("ssssddddd");
+};
+function CreatObj(name) {
+  this.name = name;
+}
+CreatObj.prototype.sayHello = function () {
+  console.log("hello" + this.name);
+};
+let obj = new CreatObj("name");
+console.log(obj, "objjjj");
+// console.log(obj.say(), "kkkk");
+console.log(CreatObj.__proto__.say(), Function.prototype.say, "kkkkk");
 var t = null;
 var t2 = null;
-// import { debounce } from "@/utils";
+import { debounce } from "@/utils";
 export default {
   data() {
     return {
       testWord: "请输入",
       testWord2: "请输入",
+      myChannel: null,
+      getMessage: "接受的消息",
     };
   },
   mounted() {
@@ -53,12 +70,33 @@ export default {
     console.log(arr4);
     arr.d();
     arr2.d();
+    this.createChannel();
+    window.addEventListener("message", (e) => {
+      console.log(e.data, "sssss");
+      this.getMessage = e.data;
+    });
+    function onResize(args) {
+      console.log(this, "this", args);
+      console.log("Window resized!");
+    }
+
+    window.addEventListener("resize", debounce(onResize, 300));
   },
   methods: {
+    sendMessage() {
+      this.myChannel.postMessage("asdfasdfasd");
+    },
+    createChannel() {
+      this.myChannel = new BroadcastChannel("demo");
+      this.myChannel.addEventListener("message", (e) => {
+        console.log(e.data, "eeeee");
+        this.getMessage = e.data;
+      });
+    },
     handleInput(val) {
       //   debounce(this.test, 500);
       if (t !== null) {
-        clearTimeout(t);
+        clearTimeout(t);   //500ms以内执行的都被清空了，间隔500ms后才执行
       }
       t = setTimeout(() => {
         console.log(val.target.value, "3");
@@ -71,7 +109,7 @@ export default {
       }
       t2 = setTimeout(() => {
         console.log(val, 44);
-        t2 = null;
+        t2 = null; //先执行一次，之后再执行都返回，等1s之后置空再执行，之后再都返回
         console.log(t2, "t22");
       }, 1000);
     },
@@ -95,5 +133,11 @@ export default {
   border: 1px solid red;
   width: 500px;
   height: 500px;
+}
+.sendMessage {
+  width: 100px;
+  border: 1px solid black;
+  padding: 5px;
+  cursor: pointer;
 }
 </style>
